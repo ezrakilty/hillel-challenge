10db5df58df33da5766b15d351ce83c310e38a87
Ezra elias kilty Cooper <ezra+github@ezrakilty.net>
2018-04-26 12:05:08 -0700

    Remove the "separate" conditions.
    The one big proof of correctness is nicer than the three separate ones. I think.

--- leftpad.v
+++ leftpad.v
@@ -10,42 +10,6 @@ Parameter char : Set.
 Definition leftpad c n (s : list char) :=
   repeat c (n - length s) ++ s.
 
-(** Proving the three correctness conditions separately. *)
-
-Lemma c1 :
-  forall c n s,
-    length (leftpad c n s) = max n (length s).
-Proof.
- unfold leftpad.
- intros.
- autorewrite with list_lemmas.
- destruct (le_lt_dec n (length s)).
-  rewrite max_r; omega.
- rewrite max_l; omega.
-Qed.
-
-Lemma c2 :
-  forall s c n,
-    allEq _ (take _ (n - length s) (leftpad c n s)) c.
-Proof.
- unfold leftpad.
- unfold allEq.
- intros.
- autorewrite with list_lemmas.
-  apply listall_repeat.
- autorewrite with list_lemmas; auto.
-Qed.
-
-Lemma c3 :
-  forall n c s,
-    drop _ (n - length s) (leftpad c n s) = s.
-Proof.
- unfold leftpad.
- intros.
- autorewrite with list_lemmas;
-   autorewrite with list_lemmas; auto.
-Qed.
-
 (** Proving all three correctness conditions at once. *)
 Lemma correctness:
   forall c n s,
